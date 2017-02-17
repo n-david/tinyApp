acc56da6f37fc54ad4888cf013c14946ebe913e8
n-david <ngdavid.93@gmail.com>
2017-02-17 09:34:48 +0000

    Added bootstrap and added error handlers

--- express_server.js
+++ express_server.js
@@ -19,8 +19,8 @@ const urlDatabase = {
     '9sm5xK': 'http://www.google.com'
   },
   'user2RandomID': {
-    iAn82x: 'http://www.facebook.com',
-    n23bIb: 'http://www.youtube.com'
+    'iAn82x': 'http://www.facebook.com',
+    'n23bIb': 'http://www.youtube.com'
   }
 };
 
@@ -30,55 +30,84 @@ const users = {
     email: 'user@example.com',
     password: bcrypt.hashSync('purple-monkey-dinosaur', 10)
   },
- 'user2RandomID': {
+  'user2RandomID': {
     id: 'user2RandomID',
     email: 'user2@example.com',
     password: bcrypt.hashSync('dishwasher-funk', 10)
   }
 };
 
-app.get('/', (req, res) => {
-  res.redirect('/urls');
-});
-
-app.get('/urls', (req, res) => {
-  console.log(urlDatabase);
-  console.log('________________________________________________________________________');
-  console.log(users);
-  console.log('________________________________________________________________________');
-  let urlDatabaseCurrent = urlDatabase;
-  if (req.session.user_id) {
-    urlDatabaseCurrent = urlDatabase[req.session.user_id];
-    res.render('urls_index', { user_current: users[req.session.user_id], urls: urlDatabaseCurrent });
-  } else {
-    res.render('urls_index_login', { user_current: users[req.session.user_id] });
+function generateRandomString() {
+  let randomString = '';
+  let alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let chars = 0;
+  while (chars < 6) {
+    randomString += alphabet[Math.floor(Math.random() * alphabet.length)];
+    chars++;
   }
-});
+  return randomString;
+}
 
-app.get('/urls/new', (req, res) => {
+app.get('/', (req, res) => {
   if (req.session.user_id) {
-    res.render('urls_new', { user_current: users[req.session.user_id] });
+    res.redirect('/urls');
   } else {
     res.redirect('/login');
   }
 });
 
+app.get('/urls', (req, res) => {
+  let urlDatabaseCurrent = urlDatabase;
+  if (req.session.user_id) {
+    urlDatabaseCurrent = urlDatabase[req.session.user_id];
+    res.render('urls_index', { userCurrent: users[req.session.user_id], urls: urlDatabaseCurrent });
+  } else {
+    res.status(401).render('urls_unauthorized', { userCurrent: users[req.session.user_id] });
+  }
+});
+
+app.get('/urls/new', (req, res) => {
+  if (req.session.user_id) {
+    res.render('urls_new', { userCurrent: users[req.session.user_id] });
+  } else {
+    res.status(401).render('urls_unauthorized', { userCurrent: users[req.session.user_id] });
+  }
+});
+
 app.get('/register', (req, res) => {
-  res.render('urls_register', { user_current: users[req.session.user_id] })
+  if (req.session.user_id) {
+    res.redirect('/');
+    return;
+  }
+  res.render('urls_register', { userCurrent: users[req.session.user_id] });
 });
 
 app.get('/login', (req, res) => {
-  res.render('urls_login', { user_current: users[req.session.user_id] })
+  if (req.session.user_id) {
+    res.redirect('/');
+    return;
+  } else {
+    res.render('urls_login', { userCurrent: users[req.session.user_id] });
+  }
 });
 
 app.get('/urls/:id', (req, res) => {
-  if (req.session.user_id){
-    if (req.params.id in urlDatabase[req.session.user_id]){
-      res.render('urls_show', { user_current: users[req.session.user_id], shortURL: req.params.id, longURL: urlDatabase[req.session.user_id][req.params.id] });
-      return;
+  for (let id in urlDatabase) {
+    if (!(req.params.id in urlDatabase[id])) {
+      continue;
+    } else if (req.session.user_id) {
+      if (req.params.id in urlDatabase[req.session.user_id]) {
+        res.render('urls_show', { userCurrent: users[req.session.user_id], shortURL: req.params.id, longURL: urlDatabase[req.session.user_id][req.params.id] });
+        return;
+      } else {
+        res.status(403).send('URL created by another user. Unauthorized to access.');
+        return;
+      }
     }
+    res.status(401).render('urls_unauthorized', { userCurrent: users[req.session.user_id] });
+    return;
   }
-  res.redirect('/');
+  res.status(404).send('URL entered is not in database.');
 });
 
 app.get('/u/:shortURL', (req, res) => {
@@ -90,49 +119,82 @@ app.get('/u/:shortURL', (req, res) => {
       }
     }
   }
-  res.redirect(urlDatabase[creator][req.params.shortURL]);
+  if (!creator) {
+    res.status(404).send('URL not found. Redirect unsuccessful.');
+  } else {
+    res.redirect(urlDatabase[creator][req.params.shortURL]);
+  }
 });
 
 app.post('/urls', (req, res) => {
-  if (req.body.longURL) {
+  if (req.session.user_id) {
     let shortURL = generateRandomString();
-    console.log(urlDatabase[req.session.user_id]);
-    if (!urlDatabase[req.session.user_id]) {
-      urlDatabase[req.session.user_id] = {};
+    if (req.body.longURL) {
+      if (!urlDatabase[req.session.user_id]) {
+        urlDatabase[req.session.user_id] = {};
+      }
+      urlDatabase[req.session.user_id][shortURL] = req.body.longURL;
     }
-    urlDatabase[req.session.user_id][shortURL] = req.body.longURL;
+    res.redirect(`/urls/${shortURL}`);
+  } else {
+    res.status(401).render('urls_unauthorized', { userCurrent: users[req.session.user_id] });
   }
-  res.redirect('/urls');
 });
 
 app.post('/urls/:id/edit', (req, res) => {
-  urlDatabase[req.session.user_id][req.params.id] = req.body.longURL;
-  res.redirect('/urls');
+  for (let id in urlDatabase) {
+    if (!(req.params.id in urlDatabase[id])) {
+      continue;
+    } else if (req.session.user_id) {
+      if (req.params.id in urlDatabase[req.session.user_id]) {
+        urlDatabase[req.session.user_id][req.params.id] = req.body.longURL;
+        res.redirect(`/urls/${req.params.id}`);
+        return;
+      } else {
+        res.status(403).send('URL created by another user. Unauthorized to access.');
+        return;
+      }
+    }
+    res.status(401).render('urls_unauthorized', { userCurrent: users[req.session.user_id] });
+    return;
+  }
+  res.status(404).send('URL entered is not in database.');
 });
 
 app.post('/urls/:id/delete', (req, res) => {
-  if (req.session.user_id) {
-    if (req.params.id in urlDatabase[req.session.user_id]) {
-      delete urlDatabase[req.session.user_id][req.params.id];
+  for (let id in urlDatabase) {
+    if (!(req.params.id in urlDatabase[id])) {
+      continue;
+    } else if (req.session.user_id) {
+      if (req.params.id in urlDatabase[req.session.user_id]) {
+        delete urlDatabase[req.session.user_id][req.params.id];
+        res.redirect('/');
+        return;
+      } else {
+        res.status(403).send('URL created by another user. Unauthorized to access.');
+        return;
+      }
     }
+    res.status(401).render('urls_unauthorized', { userCurrent: users[req.session.user_id] });
+    return;
   }
-  res.redirect('/');
+  res.status(404).send('URL entered is not in database.');
 });
 
 app.post('/login', (req, res) => {
   if (!req.body.email || !req.body.password) {
     res.status(400).send('Please enter an email and a password.');
-  return;
+    return;
   }
   for (let user in users) {
     if (req.body.email !== users[user].email) {
       continue;
     } else if (bcrypt.compareSync(req.body.password, users[user].password)) {
-        req.session.user_id = user;
-        res.redirect('/');
-        return;
+      req.session.user_id = user;
+      res.redirect('/');
+      return;
     } else {
-      res.status(403).send('Password does not match.');
+      res.status(401).send('Password does not match.');
       return;
     }
   }
@@ -142,7 +204,7 @@ app.post('/login', (req, res) => {
 
 app.post('/logout', (req, res) => {
   req.session = null;
-  res.redirect('urls/');
+  res.redirect('/');
 });
 
 app.post('/register', (req, res) => {
@@ -165,14 +227,3 @@ app.post('/register', (req, res) => {
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
-
-function generateRandomString() {
-  let randomString = '';
-  let alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let chars = 0;
-  while (chars < 6) {
-    randomString += alphabet[Math.floor(Math.random() * alphabet.length)];
-    chars++;
-  }
-  return randomString;
-}
